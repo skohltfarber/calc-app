31537cb87dfa6269cffa9002a30eac0fe255a3a5
Shawn Kohltfarber <shawn.kohltfarber@gmail.com>
2021-02-07 10:17:32 -0600

    Make a klingon calculator

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,16 @@ import { Calc } from "./calc.js";
 
 let calc = new Calc();
 
+document.getElementById('input1').addEventListener("change", function () {
+
+    document.getElementById('humaninput1').innerText = document.getElementById('input1').value;
+});
+
+document.getElementById('input2').addEventListener("change", function () {
+
+    document.getElementById('humaninput2').innerText = document.getElementById('input2').value;
+});
+
 document.getElementById('calculate').addEventListener("click", function () {
     let input1 = parseFloat(document.getElementById('input1').value);
     let input2 = parseFloat(document.getElementById('input2').value);
@@ -28,5 +38,6 @@ document.getElementById('calculate').addEventListener("click", function () {
     console.log(output);
 
     document.getElementById('output').innerText = output;
+    document.getElementById('output2').innerText = output;
 
 });
